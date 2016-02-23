c561c94ad419f5d0c009e6669bdb45e53a9fd006
rand0m1ze <luckypcsolution@gmail.com>
2016-02-23 01:39:01 -0600

    Update torscramble.sh

--- torscramble.sh
+++ torscramble.sh
@@ -6,9 +6,9 @@ SERVICE=service;
 
 if ps ax | grep -v grep | grep tor-service-defaults-torrc > /dev/null
 then
-    echo "TOR is running!"
+    echo -e "TOR is running!\e[97m"
 else
-    echo "$SERVICE is not running... starting TOR" 
+    echo -e "$SERVICE is not running... starting TOR\e[97m" 
 sudo service tor start &>/dev/null
 echo -e "Starting TOR / Please wait..."
 echo -ne '#####                     (33%)\r'
@@ -18,8 +18,17 @@ sleep 3
 echo -ne '#######################   (100%)\r'
 echo -ne '\n'
 fi
+cat << "EOF"
+ ____ ____ ____ ____ ____ ____ ____ ____ ____ 
+||S |||c |||r |||a |||m |||b |||l |||e |||r ||
+||__|||__|||__|||__|||__|||__|||__|||__|||__||
+|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|   
+EOF
+echo -e "\e[31m--------------------------------------------------------"
 echo -e "\E[1;33m::::: \e[97mTOR Identity randomizer \E[1;33m:::::\e[97m github.com/rand0m1ze \e[31m"
 read -p 'Set time in minutes to change IP "example" 1m, 10m, 25m: ' uservar
 echo -e "\E[1;33m::::: \e[97mChanging TOR identity every $uservar \E[1;33m:::::\e[31m"
 for n in {1..50}; do service tor reload && sleep $uservar; done
 echo -e "\E[1;33m::::: \e[97mHave a nice day \E[1;33m:::::\e[31m"
+
+
